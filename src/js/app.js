9962b39a8850a7830bf2ae2f96aff1d46236fe02
onaliugo <contact@onaliugo.fr>
2014-09-10 17:47:18 +0200

    Refacto all the things

--- src/js/app.js
+++ src/js/app.js
@@ -1,182 +1,149 @@
 
-var d = document
-  
-  // containers
-  , container_first  = d.getElementById("js-container_step_1")
-  , container_second = d.getElementById("js-container_step_2")
-  , container_third  = d.getElementById("js-container_step_3")
-  
-  // BtnValid
-  , BV_playersRepartiton = d.getElementById("js-valid_step1")
-  , BV_playersNames      = d.getElementById("js-valid_step2")
+var d = document;
+var $app = d.querySelector('.app');
+var animationRunning = false;
+var Players = {
+  // step 1
+  $countContainer: d.querySelector('.js-container-step-1'),
+  $countForm: d.querySelector('.js-valid-players-number'),
+  $totalPlayers: d.querySelector('.js-total-players'),
+  $byTeamPlayers: d.querySelector('.js-byteam-players'),
 
-  // inputs
-  , I_PbyTeam = d.getElementById("js-total_byteam")
-  , I_totalP  = d.getElementById("js-total_players")
-;
+  // step 2
+  $namesMainContainer: d.querySelector('.js-container-step-2'),
+  $namesForm: d.querySelector('.js-new-players-form'),
+  $namesContainer: d.querySelector('.js-new-players-container'),
 
-// http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  // step 3
+  $teamsMainContainer: d.querySelector('.js-container-step-3'),
+  $teamsContainer: d.querySelector('.js-teams-container'),
 
-var shuffleArray = function(array) {  
-  for (i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i+1))
-      , temp = array[i]
-    ;
+  generateInputs: function(){
+    for(var i=0; i<Players.total; i++){
+      var input = d.createElement("input");
+      input.classList.add("js-new-player");
+      input.placeholder = 'Player #' + (i+1);
+      this.$namesContainer.appendChild(input);
+    }
+  },
+  checkInput: function($input){
+    var val = $input.value;
+    var isNumber = !isNaN(val);
+    var isTooShort = val.length === 0;
+    var hasError = (!isNumber || isTooShort);
 
-    array[i] = array[j];
-    array[j] = temp;
-  }
-  
-  return array;
-};
-var toggleContainers = function(c1, c2){
-  c1.classList.add("hidden");
-  c2.classList.remove("hidden");
-};
+    return hasError;
+  },
+  sortTeams: function(){
+    var players = Players.names;
+    var playersLength = players.length;
+    var playersByTeam = this.$byTeamPlayers.value;
 
-var inputError = function(input){
-  var val        = input.value
-    , isTooShort = val.length < 1
-    , isNumber   = !isNaN(val)
-  ;
-
-  return !isTooShort && isNumber;
-};
-
-var validPlayerRepartition = function() {
-  var noError = false
-    , total
-    , byTeam
-  ;
-
-  I_totalP
-    .classList
-      .toggle("error", !inputError(I_totalP))
-      ;
-
-  I_PbyTeam
-    .classList
-      .toggle("error", !inputError(I_PbyTeam))
-      ;
-
-  byTeam = Math.floor(I_PbyTeam.value);
-  total  = Math.floor(I_totalP.value);
-
-  noError = !I_totalP.classList.contains("error") && !I_PbyTeam.classList.contains("error") && ((byTeam/total)<1);
-
-  if(!noError){
-    I_PbyTeam.classList.add("error");
-    I_totalP.classList.add("error");
-  }
-
-  return noError;
-};
-var generateInputs = function() {
-  var container = d.getElementById("js-new_players")
-    , size = I_totalP.value
-  ;
-
-  for (i=0; i<size; i++) {
-    var input = d.createElement("input");
-
-    input.classList.add("js-new_player");
-    input.placeholder = "Player " + (i+1);
-    container.appendChild(input);
-  }
-};
-
-var goToStep2 = function(){
-  var noError = validPlayerRepartition();
-
-  if(noError){
-    toggleContainers(container_first, container_second);
-    generateInputs();
-  }
-};
-var validAndGetPlayersNames = function(){
-  var playersArray = []
-    , inputs  = d.querySelectorAll("input.js-new_player")
-    , size    = inputs.length
-    , noError = false
-  ;
-
-  for (i=0; i<size; i++) {
-    var current = inputs[i]
-      , value   = current.value
-      , isEmpty = value.length < 1
-    ;
-
-    current.classList.toggle("error", isEmpty);
-
-    noError = !isEmpty;
-
-    if (!isEmpty)
-      playersArray.push(value);
-  }
-
-  return noError ? playersArray : false;
-};
-// http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-
-var shuffleArray = function(array) {  
-  for (i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i+1))
-      , temp = array[i]
-    ;
-
-    array[i] = array[j];
-    array[j] = temp;
-  }
-  
-  return array;
-};
-
-var sortTeam = function(array){
-
-  array = shuffleArray(array);
-
-  var container     = d.getElementById("js-teams_container")
-    , playersByTeam = I_PbyTeam.value
-    , size = array.length+1
-  ;
-
-  for (i=0; i<size; i++){
-    var player    = d.createElement("span")
-      , current   = array[i-1]
-      , isNewTeam = i % playersByTeam
-      , isEndTeam = (i/playersByTeam) % 1
-      ;
-
-    if (isNewTeam === 1) {
-      var h2 = d.createElement("h2");
-      h2.innerHTML = "Team " + Math.round((i + 1)/playersByTeam);
-      container.appendChild(h2);
+    // monster shuffle
+    for(var i=0; i<players.length; i++){
+      players = shuffleArray(players);
     }
 
-    if(i>0){
-      if (isEndTeam === 0 || i === size-1 ){
-        player.classList.add("last-item");
-        player.innerHTML = current;
+    // start da machine
+    for(var j=0; j<playersLength; j++){
+      var $newPlayer = d.createElement('span');
+      var isNewTeam = (j % playersByTeam) === 0;
+      var isEndTeam = (j % playersByTeam) === (playersByTeam - 1);
+      var isLast = j === (playersLength - 1);
+      // var isEndTeam = (i/playersByTeam) % 1;
+      // var currentPlayer = players[i-1];
+
+      $newPlayer.innerHTML = players[j];
+
+      if(isNewTeam){
+        var $h2 = d.createElement('h2');
+        var index = Math.round((j + 2)/playersByTeam);
+        $h2.innerHTML = 'Team #'+index;
+        this.$teamsContainer.appendChild($h2);
       }
-      else {
-        player.innerHTML = current + ",";
+      if(!isEndTeam && !isLast){
+        $newPlayer.innerHTML += ', ';
       }
-      container.appendChild(player);
+        this.$teamsContainer.appendChild($newPlayer);
     }
   }
 };
 
-var goToStep3 = function(){
-  var playersArray = validAndGetPlayersNames();
+var shuffleArray = function(array){
+  for(var j, x, i = array.length; i; j = parseInt(Math.random() * i), x = array[--i], array[i] = array[j], array[j] = x);
+  return array;
+};
 
-  if(playersArray){
-    toggleContainers(container_second, container_third);
-    sortTeam(playersArray);
+Players.$countForm.onsubmit = function(evt){
+  evt.preventDefault();
+  if(animationRunning) return;
+
+  var _this = Players;
+  var $totalPlayers = _this.$totalPlayers;
+  var $byTeamPlayers = _this.$byTeamPlayers;
+  var totalErrors = 0;
+
+  // reset app classes
+  $app.className = 'app';
+
+  // display error for each input
+  [$totalPlayers, $byTeamPlayers].forEach(function(el){
+    var hasError = _this.checkInput(el);
+    el.classList.toggle('error', hasError);
+    if(hasError)
+      totalErrors++;
+  });
+
+  // check if players by team are not superior at total players
+  // if not, store the total players value
+  if(Number($byTeamPlayers.value) >= Number($totalPlayers.value)){
+    $totalPlayers.classList.add('error');
+    $byTeamPlayers.classList.add('error');
+    totalErrors++;
+  }
+  else {
+    _this.total = $totalPlayers.value;
+  }
+
+  // if many errors, wobble $app
+  // if not, generate inputs for next step
+  if(totalErrors > 0)
+    $app.className += ' wobble animated';
+  else{
+    _this.$countContainer.classList.add('hidden');
+    _this.$namesMainContainer.classList.remove('hidden');
+    _this.generateInputs();
   }
 };
 
-BV_playersRepartiton
-  .addEventListener("click", goToStep2);
+Players.$namesForm.onsubmit = function(evt){
+  evt.preventDefault();
+  if(animationRunning) return;
 
-BV_playersNames
-  .addEventListener("click", goToStep3);
+  var _this = Players;
+  var $inputs = this.querySelectorAll('input');
+  Players.names = [];
+
+  // check errors
+  [].forEach.call($inputs, function(el){
+    var isEmpty = el.value.length === 0;
+    el.classList.toggle('error', isEmpty);
+    if(!isEmpty)
+      Players.names.push(el.value);
+  });
+
+  // if no errors, continue
+  if(Players.names.length == Players.total){
+    _this.$namesMainContainer.classList.add('hidden');
+    _this.$teamsMainContainer.classList.remove('hidden');
+    _this.sortTeams();
+  }
+  else {
+    $app.className += ' wobble animated';
+  }
+};
+
+$app.addEventListener('webkitAnimationEnd', function(){
+  $app.className = 'app';
+  animationRunning = false;
+});
